d6ab9980d8c618c1b82c442bf727c8072f50c5da
Desmond Rivet <desmond.rivet@gmail.com>
2019-08-18 14:38:20 -0400

    Add more checks to success case in API tests

--- micropub/test_micropub_api.py
+++ micropub/test_micropub_api.py
@@ -13,10 +13,10 @@ def setup_module():
     client = app.test_client()
     app.config['TESTING'] = True
     datef = '{published:%Y}/{published:%m}/{published:%d}'
-    timef = '{published:%HH}{published:%MM}{published:%SS}'
+    timef = '{published:%H}{published:%M}{published:%S}'
     app.config['PERMALINK_FORMAT'] = datef + '/{slug}'
     app.config['REPO_URL_ROOT'] = 'https://api.guthub.com/drivet/pelican-test-blog'
-    app.config['REPO_PATH_FORMAT'] = datef + '/' + timef
+    app.config['REPO_PATH_FORMAT'] = '/' + datef + '/' + timef + '.mpj'
 
 
 def test_get_fails_with_no_query():
@@ -48,9 +48,15 @@ def test_returns_config():
 def test_returns_success(commit_mock):
     rv = client.post('/', data={
         'content': 'hello',
-        'mp_slug': 'blub'
+        'mp_slug': 'blub',
+        'published': '2019-07-16T13:45:23.5'
     })
     assert rv.status_code == 202
+    url = app.config['REPO_URL_ROOT'] + '/2019/07/16/134523.mpj'
+    contents = '{"type": ["h-entry"], "properties": {"content": ["hello"], "mp_slug": ["blub"], "published": ["2019-07-16T13:45:23.5"]}}'
+    commit_mock.assert_called_with(url, contents)
+    print(rv.headers['Location'])
+    assert rv.headers['Location'] == 'http://localhost/2019/07/16/blub'
 
 
 def teardown_module():
