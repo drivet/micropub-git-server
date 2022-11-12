48b4bb89d5c742e63c8bf6aefc6cbd5fb6724767
Desmond Rivet <desmond.rivet@gmail.com>
2022-11-12 13:17:22 -0500

    Remove media server

--- micropub/__init__.py
+++ micropub/__init__.py
@@ -1,12 +1,10 @@
 import logging
 from flask import Flask
-from micropub.media import media_bp
 from micropub.micropub import micropub_bp
 
 
 app = Flask(__name__)
 
 
-app.register_blueprint(media_bp, url_prefix='/media')
 app.register_blueprint(micropub_bp, url_prefix='/')
 app.logger.setLevel(logging.INFO)
